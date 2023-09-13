444a4b9a92d4bbbdd032b3aeaba030b7009e0a18
preston176 <prestonnyamweya@gmail.com>
2023-09-13 09:37:20 +0300

    masking the layouts

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ function App() {
 
 
   return (
-    <>
-
-    </>
+    <div className='app'>
+  <h1>Whatsapp clone</h1>
+  <div className="app__body">
+    {/* Sidebar */}
+    {/* Chat */}
+  </div>
+    </div>
   )
 }
 
