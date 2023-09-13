7565cf5f83ff0ef16accb5a5f8a7d9b006b78e61
preston176 <prestonnyamweya@gmail.com>
2023-09-13 12:02:13 +0300

    random avatars on chats via API

--- src/Components/SidebarChat.jsx
+++ src/Components/SidebarChat.jsx
@@ -1,7 +1,23 @@
+import { Avatar } from '@mui/material'
 import './SidebarChat.css'
+import { useEffect, useState } from 'react'
 function SidebarChat() {
+    const [seed, setSeed] = useState('')
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000))
+
+    }, []);
+
+
     return (
-        <div className='sidebarChat'>SidebarChat</div>
+        <div className='sidebarChat'>
+            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+            <div className="sidebarChat__info">
+                <h2>Chat room name</h2>
+                <p>Last message ...</p>
+            </div>
+        </div>
     )
 }
 
