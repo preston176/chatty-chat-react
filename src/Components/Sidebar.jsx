8549d897e2520b1ad9ca5eecc4f2dbe28007a1d9
preston176 <prestonnyamweya@gmail.com>
2023-09-13 11:34:24 +0300

    IconButton wrapper around sidebar icons

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,16 +2,27 @@ import './Sidebar.css'
 import PersonIcon from '@mui/icons-material/Person';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import { ChatBubbleOutline, MoreVertRounded } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <div className="sidebar__header">
-                <PersonIcon />
+                <IconButton>
+                    <PersonIcon />
+                </IconButton>
                 <div className="sidebar__headerRight">
-                    <DonutLargeIcon />
-                    <ChatBubbleOutline />
-                    <MoreVertRounded />
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatBubbleOutline />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertRounded />
+                    </IconButton>
+
                 </div>
             </div>
             <div className="sidebar__search">
