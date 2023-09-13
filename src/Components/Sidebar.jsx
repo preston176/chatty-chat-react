26cbdc7ea97867842d0610327aefc4745d11cab6
preston176 <prestonnyamweya@gmail.com>
2023-09-13 10:56:16 +0300

    mui icons

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,18 +1,26 @@
 import './Sidebar.css'
+import PersonIcon from '@mui/icons-material/Person';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import { ChatBubbleOutline, MoreVertRounded } from '@mui/icons-material';
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-        <div className="sidebar__header">
+            <div className="sidebar__header">
+                <PersonIcon />
+                <div className="sidebar__headerRight">
+                    <DonutLargeIcon />
+                    <ChatBubbleOutline />
+                    <MoreVertRounded />
+                </div>
+            </div>
+            <div className="sidebar__search">
 
-        </div>
-        <div className="sidebar__search">
+            </div>
 
-        </div>
+            <div className="sidebar__chats">
 
-        <div className="sidebar__chats">
-            
-        </div>
+            </div>
         </div>
     )
 }
