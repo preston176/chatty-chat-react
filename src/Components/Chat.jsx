3d0b1fbdd65aef2fff19ba99e7050f79a9ab4111
preston176 <prestonnyamweya@gmail.com>
2023-09-15 10:07:51 +0300

    chat component structured + header avatar done

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,8 +1,26 @@
+import { Avatar } from "@mui/material"
 import "./Chat.css"
+import { useEffect, useState } from "react"
 
 function Chat() {
+    const [seed, setSeed] = useState("")
+    useEffect(() => {
+        setSeed(Math.floor(Math.random()) * 5000)
+    }, []);
+
     return (
-        <div className='chat'>Chat</div>
+        <div className='chat'>
+            <div className="chat__header">
+                {/* //icons n stuff */}
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+            </div>
+            <div className="chat__body">
+                {/* texts */}
+            </div>
+            <div className="chat__footer">
+                {/* input  */}
+            </div>
+        </div>
     )
 }
 
