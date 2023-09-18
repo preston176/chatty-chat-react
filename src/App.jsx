7248a72a363f49593b00d49c66443b494086015f
preston176 <prestonnyamweya@gmail.com>
2023-09-18 21:58:49 +0300

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,31 @@
 import './App.css'
 import Sidebar from './Components/Sidebar'
 import Chat from './Components/Chat';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
 
   return (
-    <div className='app'>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/app" element={(
+          <div className='app'>
 
-      <div className="app__body">
-        {/* Sidebar */}
-        <Sidebar />
-        <Chat />
-      </div>
-    </div>
+            <div className="app__body">
+              {/* Sidebar */}
+              <Sidebar />
+              <Chat />
+            </div>
+          </div>
+
+        )} />
+        <Route path='/' element={(
+          <h1>Home Screen</h1>
+        )} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
