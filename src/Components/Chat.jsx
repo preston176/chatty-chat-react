0ff4c85e0fb75dcc4f44c58a46263bb8ca652f84
preston176 <prestonnyamweya@gmail.com>
2023-09-19 15:44:50 +0300

    last seen functionality

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -79,7 +79,8 @@ function Chat() {
                 <div className="chat__headerInfo">
                     <h3>{roomName}</h3>
                     {/* some kind of data */}
-                    <p>Last seen at</p>
+                    <p>Last seen at {new Date(messages[messages.length - 1]?.timestamp?.toDate()).toUTCString()
+                    }</p>
                 </div>
                 <div className="chat__headerRight">
                     <IconButton>
