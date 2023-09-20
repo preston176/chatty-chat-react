364e658485fbe8709c1fbbbb924d5e63b8b79047
preston176 <prestonnyamweya@gmail.com>
2023-09-20 12:05:50 +0300

    search functionality

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,76 +1,69 @@
-import './Sidebar.css'
-import PersonIcon from '@mui/icons-material/Person';
-import DonutLargeIcon from '@mui/icons-material/DonutLarge';
-import { ChatBubbleOutline, MoreVertRounded, SearchOutlined } from '@mui/icons-material';
+import './Sidebar.css';
+import { useState, useEffect } from 'react';
 import { Avatar, IconButton } from '@mui/material';
 import SidebarChat from './SidebarChat';
-import { useState, useEffect } from 'react';
+import { useStateValue } from '../StateProvider';
 import db from '../firebase';
 import { collection, getDocs } from 'firebase/firestore';
-import { useStateValue } from '../StateProvider';
-
+import { SearchOutlined } from '@mui/icons-material';
 
 function Sidebar() {
-    const [rooms, setRooms] = useState([]);
-    const [{ user }, dispatch] = useStateValue();
-    useEffect(() => {
-        // Define an async function to fetch rooms
-        const fetchRooms = async () => {
-          try {
-            const snapshot = await getDocs(collection(db, 'rooms'));
-            const roomData = snapshot.docs.map(doc => ({
-              id: doc.id,
-              data: doc.data(),
-            }));
-            setRooms(roomData);
-          } catch (error) {
-            console.error("Error fetching rooms: ", error);
-          }
-        };
-      
-        // Call the async function to fetch rooms
-        fetchRooms();
-      
-        // Return an empty function to unsubscribe (no need for async)
-        return () => {};
-      }, []);
-      
-    return (
-        <div className='sidebar'>
-            <div className="sidebar__header">
-                <IconButton>
-                    <Avatar src={user?.photoURL} />
-               
-                </IconButton>
-                <div className="sidebar__headerRight">
-                    <IconButton>
-                        <DonutLargeIcon />
-                    </IconButton>
-                    <IconButton>
-                        <ChatBubbleOutline />
-                    </IconButton>
-                    <IconButton>
-                        <MoreVertRounded />
-                    </IconButton>
+  const [rooms, setRooms] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [{ user }, dispatch] = useStateValue();
 
-                </div>
-            </div>
-            <div className="sidebar__search">
-                <div className="sidebar__searchContainer">
-                    <SearchOutlined />
-                    <input placeholder="Search ..." type='text' />
-                </div>
-            </div>
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const snapshot = await getDocs(collection(db, 'rooms'));
+        const roomData = snapshot.docs.map(doc => ({
+          id: doc.id,
+          data: doc.data(),
+        }));
+        setRooms(roomData);
+      } catch (error) {
+        console.error("Error fetching rooms: ", error);
+      }
+    };
 
-            <div className="sidebar__chats">
-                {/* Chats */}
-                <SidebarChat addNewChat />
-                {rooms.map(room => (
-                    <SidebarChat key={room.id} id={room.id} name={room.data.name} />
-                ))}
-            </div>
+    fetchRooms();
+
+    return () => {};
+  }, []);
+
+  // Filter rooms based on search query
+  const filteredRooms = rooms.filter(room =>
+    room.data.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div className='sidebar'>
+      <div className="sidebar__header">
+        <IconButton>
+          <Avatar src={user?.photoURL} />
+        </IconButton>
+        {/* Rest of your header content */}
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input
+            placeholder="Search ..."
+            type='text'
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
         </div>
-    )
+      </div>
+      <div className="sidebar__chats">
+        {/* Chats */}
+        <SidebarChat addNewChat />
+        {filteredRooms.map(room => (
+          <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
