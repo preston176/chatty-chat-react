c78cc72c8ac40fe5872d8e72478fd040655a9500
preston176 <prestonnyamweya@gmail.com>
2023-09-20 10:01:16 +0300

    last message

--- src/Components/SidebarChat.jsx
+++ src/Components/SidebarChat.jsx
@@ -1,11 +1,31 @@
 import { Avatar } from '@mui/material'
 import './SidebarChat.css'
 import { useEffect, useState } from 'react'
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 import db from '../firebase';
 import { Link } from 'react-router-dom';
 function SidebarChat({ addNewChat, name, id }) {
     const [seed, setSeed] = useState('')
+    const [messages, setMessages] = useState("")
+
+    useEffect(() => {
+        if (id) {
+            const q = query(
+                collection(db, 'rooms', id, 'messages'),
+                orderBy('timestamp', 'desc')
+            );
+
+            const unsubscribe = onSnapshot(q, (snap) => {
+                setMessages(snap.docs.map((doc) => doc.data()));
+            });
+
+            return () => {
+                // Cleanup function to unsubscribe from the snapshot listener
+                unsubscribe();
+            };
+        }
+    }, [id]);
+
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
@@ -32,13 +52,13 @@ function SidebarChat({ addNewChat, name, id }) {
 
     return !addNewChat ? (
         <Link to={`/rooms/${id}`}>
-        <div className='sidebarChat'>
-            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-            <div className="sidebarChat__info">
-                <h2>{name}</h2>
-                <p>Last message ...</p>
+            <div className='sidebarChat'>
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+                <div className="sidebarChat__info">
+                    <h2>{name}</h2>
+                    <p>{messages[0]?.message}</p>
+                </div>
             </div>
-        </div>
         </Link>
     ) : (
         <div onClick={createChat} className='sidebarChat'>
